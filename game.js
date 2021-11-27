58aa6142b6014db9cf7b650c3bc7692fa30772aa
edpackard <edpackard81@gmail.com>
2021-11-27 11:50:08 +0000

    Refactor canvas scaling into own class

--- game.js
+++ game.js
@@ -1,5 +1,19 @@
 "use strict";
 
+const ctx = document.getElementById("canvas").getContext("2d");
+
+const height = 400;
+const width = 750;
+const windowPercentage = 0.8;
+const maxMultiplier = 20;
+
+const gameCanvas = new ScaleCanvas(
+  height,
+  width,
+  maxMultiplier,
+  windowPercentage
+);
+
 const batWidth = 20;
 const batHeight = 100;
 const playerX = 0;
@@ -10,7 +24,7 @@ function render() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.fillStyle = "#ff8080";
   ctx.fillRect(playerX, playerY, batWidth, batHeight);
-  ctx.fillRect(nWidth - batWidth, nHeight - batHeight, batWidth, batHeight);
+  ctx.fillRect(width - batWidth, height - batHeight, batWidth, batHeight);
 }
 
 function gameLoop(timeStamp) {
@@ -23,20 +37,20 @@ function update() {
   if (playerY == 0) {
     playerDown = true;
   }
-  if (playerY == nHeight - batHeight) {
+  if (playerY == height - batHeight) {
     playerDown = false;
   }
   playerDown ? playerY++ : playerY--;
 }
 
 window.addEventListener("load", () => {
-  ctx.canvas.width = cWidth;
-  ctx.canvas.height = cHeight;
-  resize();
+  ctx.canvas.width = gameCanvas.nWidth;
+  ctx.canvas.height = gameCanvas.nHeight;
+  gameCanvas.resize(ctx);
   window.requestAnimationFrame(gameLoop);
 });
 
 window.addEventListener("resize", () => {
-  resize();
+  gameCanvas.resize(ctx);
   render();
 });
---
+++ scaleCanvas.js
@@ -0,0 +1,36 @@
+"use strict";
+
+class ScaleCanvas {
+  constructor(height, width, maxMultiplier, windowPercentage) {
+    this.nHeight = height;
+    this.nWidth = width;
+    this.maxMultiplier = maxMultiplier;
+    this.windowPercentage = windowPercentage; // % of browser window taken up by canvas
+  }
+
+  resize(ctx) {
+    let cHeight = this.nHeight; // canvas height - scales when resized
+    let cWidth = this.nWidth; // canvas width - scales when resized
+    const maxWidth = this.nWidth * this.maxMultiplier;
+    const maxHeight = this.nHeight * this.maxMultiplier;
+    cWidth = window.innerWidth;
+    cHeight = window.innerHeight;
+    // ratio of the native game size width to height:
+    const nativeRatio = this.nWidth / this.nHeight;
+    const browserWindowRatio = cWidth / cHeight;
+    // browser window is too wide:
+    if (browserWindowRatio > nativeRatio) {
+      cHeight = Math.floor(cHeight * this.windowPercentage); // optional
+      if (cHeight > maxWidth) cHeight = maxHeight; // optional
+      cWidth = Math.floor(cHeight * nativeRatio);
+    } else {
+      // browser window is too high:
+      cWidth = Math.floor(cWidth * this.windowPercentage); // optional
+      if (cWidth > maxWidth) cWidth = maxWidth; // optional
+      cHeight = Math.floor(cWidth / nativeRatio);
+    }
+    // set canvas style width and height to new width and height
+    ctx.canvas.style.width = `${cWidth}px`;
+    ctx.canvas.style.height = `${cHeight}px`;
+  }
+}
