1d04bb5bd7bcba803e2c0cf8b34870f46c82df9a
edpackard <edpackard81@gmail.com>
2021-12-06 18:08:14 +0000

    Debug tests

--- spec/PongBoardSpec.js
+++ spec/PongBoardSpec.js
@@ -2,7 +2,7 @@ describe("PongBoard", function () {
   it("initialises with default values", function () {
     const board = new PongBoard();
     expect(board.gameHeight).toEqual(400);
-    expect(board.headHeight).toEqual(200);
+    expect(board.infobarHeight).toEqual(200);
     expect(board.height).toEqual(600);
     expect(board.width).toEqual(750);
     expect(board.windowPercentage).toEqual(0.8);
@@ -12,7 +12,7 @@ describe("PongBoard", function () {
   it("can accept custom values", function () {
     const board = new PongBoard(200, 200, 200, 0.5, 10);
     expect(board.gameHeight).toEqual(200);
-    expect(board.headHeight).toEqual(200);
+    expect(board.infobarHeight).toEqual(200);
     expect(board.height).toEqual(400);
     expect(board.width).toEqual(200);
     expect(board.windowPercentage).toEqual(0.5);
@@ -21,8 +21,8 @@ describe("PongBoard", function () {
 
   it("can convert y values to header values", function () {
     const board = new PongBoard();
-    expect(board.headerY(0)).toEqual(401);
-    expect(board.headerY(50)).toEqual(451);
-    expect(board.headerY(199)).toEqual(600);
+    expect(board.infobarY(0)).toEqual(401);
+    expect(board.infobarY(50)).toEqual(451);
+    expect(board.infobarY(199)).toEqual(600);
   });
 });
