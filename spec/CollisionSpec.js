c8aa180eec932717fa774021dfe042d301b33c0d
edpackard <edpackard81@gmail.com>
2021-12-07 15:46:25 +0000

    Update isColliding function to deal with X plane collisions on Y plane

--- spec/CollisionSpec.js
+++ spec/CollisionSpec.js
@@ -1,7 +1,14 @@
 describe("Collision", function () {
-  it("detects same Y plane: collision", function () {
+  it("detects same Y plane: collision on X plane: obj2 high x", function () {
     const obj1 = { x: 10, y: 10, height: 10, width: 10 };
     const obj2 = { x: 20, y: 10, height: 10, width: 10 };
+    collision = new Collision();
+    expect(collision.isColliding(obj1, obj2)).toBe(true);
+  });
+
+  it("detects same Y plane: collision on X plane: obj2 low x", function () {
+    const obj2 = { x: 10, y: 10, height: 10, width: 10 };
+    const obj1 = { x: 20, y: 10, height: 10, width: 10 };
     collision = new Collision();
     expect(collision.isColliding(obj1, obj2)).toBe(true);
   });
--- src/Collision.js
+++ src/Collision.js
@@ -1,6 +1,6 @@
 class Collision {
   isColliding(obj1, obj2) {
-    if (obj1.x >= obj2.x + obj2.width || obj2.x > obj1.x + obj1.width) {
+    if (obj1.x > obj2.x + obj2.width || obj2.x > obj1.x + obj1.width) {
       return false;
     } else {
       return true;
