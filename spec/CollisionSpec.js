f9eaf2689f904a3a51a67edf5ff1c82cb3fda3d6
edpackard <edpackard81@gmail.com>
2021-12-07 15:41:54 +0000

    Update isColliding to tackle collisions on same Y plane across X plane

--- spec/CollisionSpec.js
+++ spec/CollisionSpec.js
@@ -1,15 +1,29 @@
 describe("Collision", function () {
-  it("detects same Y plane: right to left collision", function () {
+  it("detects same Y plane: collision", function () {
     const obj1 = { x: 10, y: 10, height: 10, width: 10 };
     const obj2 = { x: 20, y: 10, height: 10, width: 10 };
     collision = new Collision();
     expect(collision.isColliding(obj1, obj2)).toBe(true);
   });
 
-  it("detects same Y plane: non-collision", function () {
-    const obj1 = { x: 10, y: 10, height: 10, width: 10 };
-    const obj2 = { x: 21, y: 10, height: 10, width: 10 };
+  it("detects same Y plane: non-collision: obj2 high x", function () {
+    const obj1 = { x: 10, y: 14, height: 10, width: 10 };
+    const obj2 = { x: 21, y: 14, height: 10, width: 10 };
     collision = new Collision();
     expect(collision.isColliding(obj1, obj2)).toBe(false);
   });
+
+  it("detects same Y plane: non-collision: obj2 low x", function () {
+    const obj1 = { x: 21, y: 25, height: 10, width: 10 };
+    const obj2 = { x: 10, y: 25, height: 10, width: 10 };
+    collision = new Collision();
+    expect(collision.isColliding(obj1, obj2)).toBe(false);
+  });
+
+  // it("detects diff Y plane: non-collision", function () {
+  //   const obj1 = { x: 10, y: 10, height: 10, width: 10 };
+  //   const obj2 = { x: 10, y: 25, height: 10, width: 10 };
+  //   collision = new Collision();
+  //   expect(collision.isColliding(obj1, obj2)).toBe(false);
+  // });
 });
--- src/Collision.js
+++ src/Collision.js
@@ -1,9 +1,9 @@
 class Collision {
   isColliding(obj1, obj2) {
-    if (obj1.x + obj1.width >= obj2.x) {
-      return true;
-    } else {
+    if (obj1.x >= obj2.x + obj2.width || obj2.x > obj1.x + obj1.width) {
       return false;
+    } else {
+      return true;
     }
   }
 }
